4bfbfb6796f3038ed38f9dc0522695dab36686c0
Ayoub21061 <21061@ecam.be>
2026-09-23 20:25:43 +0200

    Test update into v2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
-  res.end('Version 1 - ' + new Date().toISOString());
+  res.end('Version 2 - ' + new Date().toISOString());
 });
 server.listen(3000, () => console.log('Listening on 3000'));
