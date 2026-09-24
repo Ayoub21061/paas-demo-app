f05396fb2c77dd355d75665c5cea202c7ed2e522
Ayoub21061 <21061@ecam.be>
2026-09-24 09:11:41 +0200

    v4

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
-  res.end('Version 3 - ' + new Date().toISOString());
+  res.end('Version 4 - ' + new Date().toISOString());
 });
 server.listen(3000, () => console.log('Listening on 3000'));
